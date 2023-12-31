34d274a647ee74bb59a9b3c87f4ab5e1f2e8f2bb
Debajit Majumder <debajit.business@gmail.com>
2023-12-31 17:54:06 +0530

    Basic maps usage and update the properties of map in Go

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	colors := map[string]string {
+		"white": "#ffffff",
+	}
+	fmt.Println(colors)
+
+	// Another way to declare a map
+	var colors1 map[string]string
+	fmt.Println(colors1)
+
+	// Another way to declare a map using make
+	colors2 := make(map[string]string)
+	fmt.Println(colors2)
+
+	// Update/Add value to map
+	colors2["white"] = "white"
+	fmt.Println(colors2)
+
+	colors["white"] = "white"
+	fmt.Println(colors)
+}
